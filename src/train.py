005008d3850664d82845bf8b8d36332fabbef262
Kumar Aryan <69475131+witharyank@users.noreply.github.com>
2026-02-04 10:01:30 +0530

    most accurate 94.1%

--- src/train.py
+++ src/train.py
@@ -1,15 +1,14 @@
 from ultralytics import YOLO
 
-
 def main():
-    model = YOLO("yolov8n.pt")
+    model = YOLO("yolov8m.pt")  # upgrade model
 
     model.train(
         data="C:/Users/krary/OneDrive/Desktop/datasets/coco128_enhanced/coco128_enhanced.yaml",
         epochs=40,
-        imgsz=640
+        imgsz=640,
+        batch=8  
     )
-
 
 if __name__ == "__main__":
     main()
