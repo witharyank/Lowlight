3adb1db16c195eac64159016484ccaa70ee26a5a
Kumar Aryan <69475131+witharyank@users.noreply.github.com>
2026-02-04 11:07:44 +0530

    map95 reached 83%

--- src/train.py
+++ src/train.py
@@ -1,13 +1,16 @@
 from ultralytics import YOLO
 
 def main():
-    model = YOLO("yolov8m.pt")  # upgrade model
+    model = YOLO("yolov8l.pt")
 
     model.train(
         data="C:/Users/krary/OneDrive/Desktop/datasets/coco128_enhanced/coco128_enhanced.yaml",
         epochs=40,
         imgsz=640,
-        batch=8  
+        batch=4,
+        cos_lr=True,
+        patience=10,
+        name="yolov8l_enhanced"
     )
 
 if __name__ == "__main__":
