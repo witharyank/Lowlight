e037520d5cae5c5c24eade82e2aca69cf8192401
Kumar Aryan <69475131+witharyank@users.noreply.github.com>
2026-02-09 22:00:57 +0530

    changes all things

--- src/train.py
+++ src/train.py
@@ -1,17 +1,23 @@
 from ultralytics import YOLO
 
 def main():
-    model = YOLO("yolov8l.pt")
+    model = YOLO("yolov8m.pt")  # balanced power
 
     model.train(
         data="C:/Users/krary/OneDrive/Desktop/datasets/coco128_enhanced/coco128_enhanced.yaml",
-        epochs=40,
-        imgsz=640,
+        epochs=50,
+        imgsz=768,
         batch=4,
         cos_lr=True,
         patience=10,
-        name="yolov8l_enhanced"
+        hsv_h=0.02,
+        hsv_s=0.8,
+        hsv_v=0.6,
+        mosaic=0.5,
+        mixup=0.1,
+        name="v8m_768_enhanced"
     )
+
 
 if __name__ == "__main__":
     main()
